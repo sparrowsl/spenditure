00c2c0af827239e7c3a6fa4ba03af7c4229667b1
benjithorpe <benthorpe.dev@gmail.com>
2023-02-18 07:53:21 +0000

    form to add new employees

---
+++ src/routes/(admin)/employees/add/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Input from '../../../../lib/components/shared/Input.svelte';
+	import Button from '../../../../lib/components/shared/Button.svelte';
+</script>
+
+<section class="min-h-[90vh] bg-white p-5">
+	<h1 class="mb-5 text-3xl font-bold text-gray-700">Add Employee</h1>
+
+	<form action="" method="POST" class="max-w-xl">
+		<fieldset class="grid gap-3">
+			<div>
+				<label for="" class="text-gray-700">Name</label>
+				<Input name="name" placeholder="Full name" required={true} />
+			</div>
+
+			<div>
+				<label for="" class="text-gray-700">Email</label>
+				<Input type="email" name="email" placeholder="Email (optional)" />
+			</div>
+
+			<div>
+				<label for="" class="text-gray-700">Contact</label>
+				<Input type="tel" name="contact" placeholder="Contact" />
+			</div>
+
+			<div class="flex gap-10">
+				<a href="/employees" class="mt-5 w-fit rounded bg-teal-700 py-2 px-4 text-sm text-white">
+					Back
+				</a>
+				<Button type="submit" classes="bg-gray-700 w-fit text-sm text-white p-2 rounded mt-5">
+					Add Employee
+				</Button>
+			</div>
+		</fieldset>
+	</form>
+</section>
